0ce02132f6cebbcd45f43fd8f4a6a381e2d8cf8f
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-01 08:16:40 +0600

    first commit

---
+++ src/api/utils.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// upload image and return img url
+const imageUpload = async (imageData) => {
+  const formData = new FormData();
+  formData.append("image", imageData);
+
+  const { data } = await axios.post(
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
+    formData
+  );
+  return data.data.display_url;
+};
+
+export default imageUpload;
