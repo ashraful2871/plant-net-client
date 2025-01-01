4da69973ec0e3d12944a0704a249b10869aa2c55
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-01 09:30:18 +0600

    add planet in db and show in ui

--- src/components/Home/Plants.jsx
+++ src/components/Home/Plants.jsx
@@ -1,14 +1,37 @@
-import Card from './Card'
-import Container from '../Shared/Container'
+import Card from "./Card";
+import Container from "../Shared/Container";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import LoadingSpinner from "../Shared/LoadingSpinner";
 
 const Plants = () => {
+  const { data: plants, isLoading } = useQuery({
+    queryKey: ["plants"],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_API_URL}/plants`
+      );
+      return data;
+    },
+  });
+  console.log(plants);
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  }
+
   return (
     <Container>
-      <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <Card />
-      </div>
+      {plants && plants.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {plants.map((plant) => (
+            <Card key={plant._id} plant={plant}></Card>
+          ))}
+        </div>
+      ) : (
+        <p>no data found</p>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default Plants
+export default Plants;
--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,59 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import imageUpload from "../../../api/utils";
+import { useContext, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
+import axios from "axios";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const [uploadButtonTExt, setUploadButtonText] = useState("upload image");
+  const [loading, setLoading] = useState(false);
+  const axiosSecure = useAxiosSecure();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const imageUrl = await imageUpload(image);
+
+    //seller info
+    const seller = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+
+    //create plant data in object
+    const plantData = {
+      name,
+      description,
+      category,
+      price,
+      quantity,
+      imageUrl,
+      seller,
+    };
+    console.table(plantData);
+
+    //save plat in db
+    try {
+      await axiosSecure.post("/plants", plantData);
+      toast.success("data added successfully");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <Helmet>
@@ -9,9 +61,14 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uploadButtonTExt={uploadButtonTExt}
+        setUploadButtonText={setUploadButtonText}
+        loading={loading}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
